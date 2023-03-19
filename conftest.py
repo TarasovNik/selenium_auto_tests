471b4ef3be15cf13024ac87e94eb21dd7ce6efb4
TarasovNik <57790144+TarasovNik@users.noreply.github.com>
2023-03-19 21:26:11 +0300

    FIX: add assert for test

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default="None",
-                     help="Choose language: chrome or firefox")
+                     help="Choose language for site")
 
 @pytest.fixture(scope="function")
 def browser(request):
